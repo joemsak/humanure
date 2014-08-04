6df7f2242e921bcb24ca19789eba8ddc16d02713
Ryan Lupinski <ryanlupinski@Macintosh-3.local>
2014-08-04 19:24:44 -0400

    Update humanure to use new Mention with elixir

--- humanure.py
+++ humanure.py
@@ -2,30 +2,31 @@
 # -*- coding: utf-8 -*-
  
 from twython import Twython
+from mention import *
 import time
+
+setup_all()
+create_all()
 
 CONSUMER_KEY    = 'iAsmYLEOXvcUKmBdB31UnSf39'
 CONSUMER_SECRET = 'x5pgFtC1a7GLXQeGbUn1rVkO4p1XOcRQViY12ii1Ab9u6umlQR'
 OAUTH_TOKEN     = '501407305-QW818odRc9DKuUqCqZOAXwKu7p091C3XJlp7la8i'
 OAUTH_SECRET    = 'RD4ShNSCCAwsTkqAzaNS17BFCtSPUTxEkaPcRx6Muetq6'
 
-twitter  = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET)  
-img      = open("album_artwork.jpeg", 'rb')
-filepath = 'tmp/replied_status_ids.txt'
+twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET)  
+img = open("album_artwork.jpeg", 'rb')
 
 while True:
-  mention   = twitter.get_mentions_timeline()[0]
-  status_id = mention['id']
-  statuses  = open(filepath).read() # read the file here
- 
-  if str(status_id) not in statuses:
-    screen_name = '@' + mention['user']['screen_name']
-    twitter.update_status_with_media(status = screen_name,
-                                     media  = img,
-                                     in_reply_to_status_id = status_id)
-    with open(filepath, 'a') as database: # open the file a second time for writing
-      database.write(str(status_id) + "\n")
-  else:
-    print 'We replied to this one already!'
-    
+  for mention in twitter.get_mentions_timeline(): 
+    status_id = str(mention['id'])
+    if not Mention.query.filter_by(status_id=status_id).all():
+      screen_name = '@' + mention['user']['screen_name']
+      twitter.update_status_with_media(status = screen_name,
+                                       media  = img,
+                                       in_reply_to_status_id = status_id)
+      Mention(status_id=status_id) # creates new mention
+    else:
+      print 'We replied to this one already!'
+
+  session.commit() # saves all Mentions created in loop
   time.sleep(60) # 1 minute
