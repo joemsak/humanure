c431c23371130d6a702a1cab88e0b61b6d6684b1
Joe Sak <joe@joesak.com>
2014-08-04 20:08:29 -0400

    Fix whitespace, save mentions each loop of mentions

--- humanure.py
+++ humanure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 from twython import Twython
 from mention import *
 import time
@@ -13,20 +13,20 @@ CONSUMER_SECRET = 'x5pgFtC1a7GLXQeGbUn1rVkO4p1XOcRQViY12ii1Ab9u6umlQR'
 OAUTH_TOKEN     = '501407305-QW818odRc9DKuUqCqZOAXwKu7p091C3XJlp7la8i'
 OAUTH_SECRET    = 'RD4ShNSCCAwsTkqAzaNS17BFCtSPUTxEkaPcRx6Muetq6'
 
-twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET)  
+twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET)
 img = open("album_artwork.jpeg", 'rb')
 
 while True:
-  for mention in twitter.get_mentions_timeline(): 
+  for mention in twitter.get_mentions_timeline():
     status_id = str(mention['id'])
     if not Mention.query.filter_by(status_id=status_id).all():
       screen_name = '@' + mention['user']['screen_name']
       twitter.update_status_with_media(status = screen_name,
                                        media  = img,
                                        in_reply_to_status_id = status_id)
-      Mention(status_id=status_id) # creates new mention
+      Mention(status_id=status_id) # builds new mention
+      session.commit() # saves Mention
     else:
       print 'We replied to this one already!'
 
-  session.commit() # saves all Mentions created in loop
   time.sleep(60) # 1 minute
--- mention.py
+++ mention.py
@@ -1,10 +1,10 @@
-import sqlalchemy.orm 
+import sqlalchemy.orm
 sqlalchemy.orm.ScopedSession = sqlalchemy.orm.scoped_session
 from elixir import *
 import os
- 
+
 metadata.bind = os.environ['DATABASE_URL']
 metadata.bind.echo = True
- 
+
 class Mention(Entity):
   status_id = Field(String)
